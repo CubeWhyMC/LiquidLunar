2c30d451dced4def78679ca7bd7f6c7e38ed1f78
CubeWhy <cubewhy@cubewhy.eu.org>
2023-06-22 14:29:18 +0800

    [未测试] 加入LunarCN ModLoader

---
+++ src/main/java/org/cubewhy/lunarcn/LunarCnMod.java
@@ -0,0 +1,10 @@
+package org.cubewhy.lunarcn;
+
+import org.cubewhy.lunarcn.loader.api.ModInitializer;
+
+public class LunarCnMod implements ModInitializer {
+    @Override
+    public void preInit() {
+
+    }
+}

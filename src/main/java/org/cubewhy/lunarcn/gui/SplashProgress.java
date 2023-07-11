7d884752d5174a317e482a2ae49c34bcbf97d824
CubeWhy <cubewhy@cubewhy.eu.org>
2023-07-11 12:17:41 +0800

    new icons

--- src/main/java/org/cubewhy/lunarcn/gui/SplashProgress.java
+++ src/main/java/org/cubewhy/lunarcn/gui/SplashProgress.java
@@ -24,6 +24,8 @@ public class SplashProgress {
     private static ResourceLocation splash;
     private static UnicodeFontRenderer unicodeFontRenderer;
 
+    private static int renderProgress = 0;
+
     public static void update() {
         if (Minecraft.getMinecraft() == null || mc.getLanguageManager() == null) {
             return;
@@ -52,7 +54,7 @@ public class SplashProgress {
 
         GlStateManager.matrixMode(GL11.GL_PROJECTION);
         GlStateManager.loadIdentity();
-        GlStateManager.ortho(0.0D, (double) scaledResolution.getScaledWidth(), (double) scaledResolution.getScaledHeight(), 0.0D, 1000.0D, 3000.0D);
+        GlStateManager.ortho(0.0D, scaledResolution.getScaledWidth(), scaledResolution.getScaledHeight(), 0.0D, 1000.0D, 3000.0D);
         GlStateManager.matrixMode(GL11.GL_MODELVIEW);
         GlStateManager.loadIdentity();
         GlStateManager.translate(0.0F, 0.0F, -2000.0F);
@@ -96,6 +98,8 @@ public class SplashProgress {
         double nProgress = (double) progress;
         double calculation = (nProgress / MAX) * scaledResolution.getScaledWidth();
 
+        renderProgress = (int) calculation;
+
         Gui.drawRect(0, scaledResolution.getScaledHeight() - 35, scaledResolution.getScaledWidth(), scaledResolution.getScaledHeight(), new Color(0, 0, 0, 50).getRGB());
 
         GlStateManager.resetColor();
@@ -109,7 +113,7 @@ public class SplashProgress {
         GlStateManager.resetColor();
         resetTextureState();
 
-        Gui.drawRect(0, scaledResolution.getScaledHeight() - 2, (int) calculation, scaledResolution.getScaledHeight(), new Color(149, 201, 144).getRGB());
+        Gui.drawRect(0, scaledResolution.getScaledHeight() - 2, renderProgress, scaledResolution.getScaledHeight(), new Color(149, 201, 144).getRGB());
 
         Gui.drawRect(0, scaledResolution.getScaledHeight() - 2, scaledResolution.getScaledWidth(), scaledResolution.getScaledHeight(), new Color(0, 0, 0, 10).getRGB());
     }
